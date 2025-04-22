baaf369bc20a9424f37e9a1cc470760f33799b5f
okallo <clayne.co25@gmail.com>
2025-04-23 01:55:30 +0300

    allowed vercel domain access to the backend

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://pawa-solutions-entry.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
